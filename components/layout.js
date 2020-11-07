360b7e5ccb37d7dff9e8b94e587a0c99f3d27ff8
babono <muhammad.akbar@gmail.com>
2020-11-08 03:00:51 +0700

    add public folder

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
+import styles from './layout.module.scss'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'MYREI'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Welcome to MYREI'
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
-      <Head>
+    <>
+    <Head>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet"></link>
         <meta
@@ -25,34 +25,11 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+    <div className={styles.wrapper}>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/logo.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
+        <div className={styles.container}>
+          <Link href="/" className={styles.headerLogo}>{name}</Link>
+        </div>
       </header>
       <main>{children}</main>
       {!home && (
@@ -62,6 +39,10 @@ export default function Layout({ children, home }) {
           </Link>
         </div>
       )}
+      <footer>
+        Copyrihgt 
+      </footer>
     </div>
+    </>
   )
 }
